d8879a73a2e13194310401ab92812fad8114efb2
daniil <danielov.dm@dns-shop.ru>
2023-04-29 18:33:40 +0500

    small environment edits

--- app/environments/dev/common/config/main-local.php
+++ app/environments/dev/common/config/main-local.php
@@ -1,43 +1,20 @@
 <?php
 
+use yii\db\Connection;
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php')
+);
+
 return [
     'components' => [
-        'db' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],
-        'mailer' => [
-            'class' => \yii\symfonymailer\Mailer::class,
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default.
-            'useFileTransport' => true,
-            // You have to set
-            //
-            // 'useFileTransport' => false,
-            //
-            // and configure a transport for the mailer to send real emails.
-            //
-            // SMTP server example:
-            //    'transport' => [
-            //        'scheme' => 'smtps',
-            //        'host' => '',
-            //        'username' => '',
-            //        'password' => '',
-            //        'port' => 465,
-            //        'dsn' => 'native://default',
-            //    ],
-            //
-            // DSN example:
-            //    'transport' => [
-            //        'dsn' => 'smtp://user:pass@smtp.example.com:25',
-            //    ],
-            //
-            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
-            // Or if you use a 3rd party service, see:
-            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
+        'db'       => [
+            'class' => Connection::class,
+            'username'     => 'user',
+            'password'     => 'password',
+            'dsn'          => 'pgsql:host=pgsql;port=5432;dbname=postgres',
+            'charset'      => 'utf8',
         ],
     ],
 ];
